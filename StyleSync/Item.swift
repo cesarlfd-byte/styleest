1ed376666c502969cc62c6bb84c47614006880ba
Cesar <cesar.lfd@icloud.com>
2026-01-03 19:55:02 -0300

    Initial Commit

---
+++ StyleSync/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  StyleSync
+//
+//  Created by Cesar Gama Camboim on 03/01/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ StyleSync/StyleSyncApp.swift
@@ -0,0 +1,32 @@
+//
+//  StyleSyncApp.swift
+//  StyleSync
+//
+//  Created by Cesar Gama Camboim on 03/01/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StyleSyncApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
